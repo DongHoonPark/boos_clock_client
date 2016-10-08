61429e4d2d2875a4b4bcea1787e201774bb43558
donghoonpark <donghun94@gmail.com>
2016-10-09 03:19:12 +0900

    donation&developer page changed

--- src/app/developer/developer.component.ts
+++ src/app/developer/developer.component.ts
@@ -1,13 +1,17 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'developer',
   templateUrl: 'developer.component.html'
 })
 export class DeveloperComponent implements OnInit {
-  constructor() {
+  constructor(private router:Router) {
   }
 
   ngOnInit() {
+  }
+  route(to:string){
+    this.router.navigate(['/'+to]);
   }
 }
